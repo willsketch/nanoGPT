55fc3025a4b624a341cccb0caae3f4f4dc59c13c
william mwine <williammwine219@gmail.com>
2023-02-16 00:07:16 +0100

    modified api

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, UploadFile, File, Response
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import PlainTextResponse, StreamingResponse
 
 
 app = FastAPI()
@@ -12,11 +13,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get('/')
+#TODO this shoud take in a variable 'CAT' of 'FTT'
+# and return questions generated from sample.py
+async def question_streamer():
+    n = 10
+    while n > 0:
+        n -= 1
+        yield (str(n) + "\n")
+
+#TODO should be able to generate all subject questions from
+# one function otherwise multiple functions
+@app.get('/', response_class=PlainTextResponse)
 def root():
-    return {"message": "Hello World"}
-
-
-@app.post("/uploadfile/")
-def upload_video(file: UploadFile):
-    return Response(content=file.filename)
+    return StreamingResponse(question_streamer())
